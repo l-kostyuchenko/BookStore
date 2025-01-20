da9d9dd56cc87afe5d1f7548a74748d6fe14e323
lidiya_kostyuchenko <lidiya.kostyuchenko@simbirsoft.com>
2025-01-20 12:42:32 +0400

    docker compose

---
+++ BookStore.Infrastructure/Extensions/MigrateExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure.Extensions
+{
+	public static class MigrateExtensions
+	{
+		public static void UseDBMigration(this IServiceProvider provider)
+		{
+			using var scope = provider.CreateScope();
+			var dbcontext = scope.ServiceProvider.GetRequiredService<BookStoreContext>();
+			dbcontext.Database.Migrate();
+		}
+	}
+}
